d9de4b9b1463664f264ceb1a8f42456c091a433e
JoshRiebe <joriebe@wisc.edu>
2021-03-22 01:00:57 -0500

    Activity 8

---
+++ boilerplate/js/bubblechart.js
@@ -0,0 +1,156 @@
+//execute script when window is loaded
+window.onload = function(){
+
+    var w = 900, h = 500;    
+    var container = d3.select("body") //get the <body> element from the DOM
+        .append("svg") //put a new svg in the body
+        .attr("width", w) //assign the width
+        .attr("height", h) //assign the height
+        .attr("class", "container") //always assign a class (as the block name) for styling and future selection
+        .style("background-color", "rgba(0,0,0,0.2)"); //only put a semicolon at the end of the block!
+
+    //innerRect block
+    var innerRect = container.append("rect") //put a new rect in the svg
+    .datum(400)    
+    .attr("width", function(d){
+        return d * 2;
+    })
+    .attr("height", function(d){
+        return d;
+    }) 
+    .attr("class", "innerRect")
+    .attr("x", 50) //position from left on the x (horizontal) axis
+    .attr("y", 50) //position from top on the y (vertical) axis
+    .style("fill", "#FFFFFF"); //fill color
+
+    //cityPop array
+    var cityPop = [
+        { 
+            city: 'Madison',
+            population: 233209
+        },
+        {
+            city: 'Milwaukee',
+            population: 594833
+        },
+        {
+            city: 'Green Bay',
+            population: 104057
+        },
+        {
+            city: 'Superior',
+            population: 27244
+        }
+    ];
+
+    //find the minimum value of the array
+    var minPop = d3.min(cityPop, function(d){
+        return d.population;
+    });
+
+    //find the maximum value of the array
+    var maxPop = d3.max(cityPop, function(d){
+        return d.population;
+    });
+
+    //scale for circles center y coordinate
+    var y = d3.scaleLinear()
+        .range([450, 50])
+        .domain([0, 700000]);
+
+    //scale stuff
+    var x = d3.scaleLinear() //create the scale
+        .range([90, 810]) //output min and max
+        .domain([0, 3.3]); //input min and max
+    
+    //color scale generator 
+    var color = d3.scaleLinear()
+        .range([
+            "#FDBE85",
+            "#D94701"
+        ])
+        .domain([
+            minPop, 
+            maxPop
+        ]);    
+    
+    //circle stuff
+    var circles = container.selectAll(".circles")
+        .data(cityPop)
+        .enter()
+        .append("circle")
+        .attr("class", "circles")
+        .attr("id", function(d){
+            return d.city;
+        })
+        .attr("r", function(d){ //circle radius
+            var area = d.population * .01;
+            return Math.sqrt(area/Math.PI);
+        })
+        .attr("cx", function(d, i){ //x coordinate using scale generator
+            return x(i);
+        })
+        .attr("cy", function(d){ //y coordinate using y scale
+            return y(d.population);
+        })
+        .style("fill", function(d, i){
+            return color(d.population);
+        })
+        .style("stroke", "#000");
+
+    //y axis generator
+    var yAxis = d3.axisLeft(y);
+
+    //create axis g element and add axis
+    var axis = container.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(50, 0)");
+    
+    yAxis(axis);
+
+    //title
+    var title = container.append("text")
+        .attr("class", "title")
+        .attr("text-anchor", "middle")
+        .attr("x", 450)
+        .attr("y", 30)
+        .text("Four Wisconsin City Populations");
+
+    //circle label position
+    var labels = container.selectAll(".labels")
+        .data(cityPop)
+        .enter()
+        .append("text")
+        .attr("class", "labels")
+        .attr("text-anchor", "left")
+        .attr("y", function(d){
+            //vertical position centered on each circle
+            return y(d.population);
+        });
+
+    //first line of labels
+    var nameLine = labels.append("tspan")
+        .attr("class", "nameLine")
+        .attr("x", function(d,i){
+            //horizontal position to the right of each circle
+            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+        })
+        .text(function(d){
+            return d.city;
+        });
+    
+        //format generator
+        var format = d3.format(",");
+    
+        //second line of label
+    var popLine = labels.append("tspan")
+        .attr("class", "popLine")
+        .attr("x", function(d,i){
+            //horizontal position to the right of each circle
+            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+        })
+        .attr("dy", "15") //vertical offset
+        .text(function(d){
+            return "Pop. " + format(d.population);
+        });
+};
